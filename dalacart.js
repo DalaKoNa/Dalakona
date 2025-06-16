219cce35a632a7c72e305be299992435c247f89d
Dala Ko Na Service <dalakona.service@gmail.com>
2025-06-16 22:26:35 +0800

    dalacart.js

--- dalacart.js
+++ dalacart.js
@@ -1,1 +1,36 @@
+let cart = [];
+
+function addToCart(item) {
+  const existingItem = cart.find(i => i.name === item.name);
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.push({ ...item, quantity: 1 });
+  }
+  renderCart();
+}
+
+function removeFromCart(index) {
+  cart.splice(index, 1);
+  renderCart();
+}
+
+function updateQuantity(index, amount) {
+  cart[index].quantity += amount;
+  if (cart[index].quantity <= 0) cart.splice(index, 1);
+  renderCart();
+}
+
+function renderCart() {
+  const cartContainer = document.getElementById('cart-items');
+  const totalPriceContainer = document.getElementById('cart-total');
+  cartContainer.innerHTML = '';
+  let total = 0;
+
+  cart.forEach((item, index) => {
+    const itemTotal = item.price * item.quantity;
+    total += itemTotal;
+    cartContainer.innerHTML += `
+      <div class="cart-item">
+        <span>${item.name} (₱${item.price}) x
 
